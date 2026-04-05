e650a164aed861892c7d83a52f07346aafa0320e
dipanjan-adhikari-912 <dipanjan.can1@gmail.com>
2026-04-05 22:20:20 +0530

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.0-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
